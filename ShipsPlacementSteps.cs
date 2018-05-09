adbb4b572d0443ae06c50bd6dfdcc52d9b6c8fdb
Mihail0 <bms-7@mail.ru>
2018-05-09 16:01:44 +0700

    Initial commit

---
+++ ShipsPlacementSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SeaBattleBDD
+{
+    [Binding]
+    public class ShipsPlacementSteps
+    {
+        [Given(@"I have a sea battle map")]
+        public void GivenIHaveASeaBattleMap()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"the map is empty")]
+        public void GivenTheMapIsEmpty()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I put a ship at origin")]
+        public void WhenIPutAShipAtOrigin()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"map should contains one ship at origin")]
+        public void ThenMapShouldContainsOneShipAtOrigin()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}

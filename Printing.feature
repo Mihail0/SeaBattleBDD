ff8ef77d26644bba994d98d910434345850016be
Mihail0 <bms-7@mail.ru>
2018-05-11 19:29:20 +0700

    Added printing feature

---
+++ Printing.feature
@@ -0,0 +1,13 @@
+﻿Feature: Printing
+	In order to display map
+	As a sea battle game engine
+	I want to print map on the screen to see happened
+
+Scenario: Printing at the beginning
+	Given I have a ships map
+	And ships map is empty
+	And I have a shots map
+	And shots map is empty
+	When I make the result map
+	And I print the result map
+	Then the output should be all water
---
+++ PrintingSteps.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SeaBattleBDD
+{
+    [Binding]
+    public class PrintingSteps
+    {
+        [Given(@"I have a ships map")]
+        public void GivenIHaveAShipsMap()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"ships map is empty")]
+        public void GivenShipsMapIsEmpty()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have a shots map")]
+        public void GivenIHaveAShotsMap()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"shots map is empty")]
+        public void GivenShotsMapIsEmpty()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I make the result map")]
+        public void WhenIMakeTheResultMap()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I print the result map")]
+        public void WhenIPrintTheResultMap()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the output should be all water")]
+        public void ThenTheOutputShouldBeAllWater()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}

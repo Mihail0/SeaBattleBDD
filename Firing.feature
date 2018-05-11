5e8c38ec7b99f2abaf2145a8fdba6846febd3398
Mihail0 <bms-7@mail.ru>
2018-05-11 18:13:54 +0700

    Added Firing feature

---
+++ Firing.feature
@@ -0,0 +1,10 @@
+﻿Feature: Firing
+	In order to destroy all ships
+	As a sea battle game engine
+	I want to break every ship on map
+
+Scenario: Firing at origin of empty map
+	Given I have a map of shots
+	And the map of shots is empty
+	When I'm shooting at origin
+	Then map of shots should contains one shot at origin
---
+++ FiringSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SeaBattleBDD
+{
+    [Binding]
+    public class FiringSteps
+    {
+        [Given(@"I have a map of shots")]
+        public void GivenIHaveAMapOfShots()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"the map of shots is empty")]
+        public void GivenTheMapOfShotsIsEmpty()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I'm shooting at origin")]
+        public void WhenIMShootingAtOrigin()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"map of shots should contains one shot at origin")]
+        public void ThenMapOfShotsShouldContainsOneShotAtOrigin()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}

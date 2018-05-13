0c2d5dc66c47da00ac089f53bac702f44262b2bf
Mihail0 <bms-7@mail.ru>
2018-05-13 13:33:09 +0700

    Added random function

--- defs.cs
+++ defs.cs
@@ -8,11 +8,18 @@ namespace SeaBattleBDD
 {
     public static class Globals
     {
+        private static readonly Random random = new Random();
+
         public const byte MAPSIZE = 10;
         public const bool EMPTY = false;
         public const bool SHIP = true;
         public const bool SHOT = true;
 
         public const char WATER = '*';
+
+        public static byte getRandom(byte min, byte max)
+        {
+            return Convert.ToByte(random.Next(min, max));
+        }
     }
 }
